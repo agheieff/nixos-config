fd74f21cd2bb7fef625cf068c2edb9db88592825
agheieff <agheieff@pm.me>
2025-01-09 20:30:06 +0100

    gitignore

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Ensure we're in the right directory
 cd /etc/nixos
-
-# Copy configuration files
 sudo cp -r ~/nixos/* /etc/nixos/
-
-# Rebuild the system using flakes
 sudo nixos-rebuild switch --flake .#nixos
